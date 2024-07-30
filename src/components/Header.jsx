d0c714308282400b739cda4be7b395fcc475c5f3
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-30 16:55:55 -0500

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Social Media Dashboard</h1>
+      <p>Total Followers: 23,004</p>
+      <hr />
+      <div className="flex place-content-between">
+        <p>Dark Mode</p>
+        <input type="checkbox" />
+      </div>
+    </header>
+  )
+}
+
+export default Header
