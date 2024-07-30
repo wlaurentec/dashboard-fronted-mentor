abd03c3901b6f4ac9d54148864e6caa921030313
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-30 17:47:14 -0500

    add header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
-
 const Header = () => {
   return (
-    <header>
-      <h1>Social Media Dashboard</h1>
-      <p>Total Followers: 23,004</p>
-      <hr />
-      <div className="flex place-content-between">
-        <p>Dark Mode</p>
-        <input type="checkbox" />
+    <header className="bg-Very-Pale-Blue h-[235px] rounded-b-[20px] p-8">
+      <h1 className="text-Very-Dark-Blue text-2xl font-bold mb-1 ">
+        Social Media Dashboard
+      </h1>
+      <p className="text-Dark-Grayish-Blue font-bold mb-6">
+        Total Followers: 23,004
+      </p>
+      <hr className="bg-black mb-5" />
+      <div className="flex justify-between">
+        <p className="text-Dark-Desaturated-Blue font-bold">Dark Mode</p>
+        <label htmlFor="darkmode" className="border bg-Toggle w-12 h-6 rounded-full cursor-pointer p-1">
+          <input type="checkbox" id="darkmode" className="peer sr-only"/>
+          <div className="w-4 h-4 rounded-full bg-Light-Grayish-Blue peer-checked:translate-x-6 transition-all"></div>
+        </label>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "Lime-Green": 'hsl(163, 72%, 41%)',
+        "Bright-Red": 'hsl(356, 69%, 56%)',
+        "Facebook": 'hsl(208, 92%, 53%)',
+        "Twitter": 'hsl(203, 89%, 53%)',
+        // "Instagram-From": 'hsl(37, 97%, 70%)',
+        // InstagramTo: 'hsl(329, 70%, 58%)',
+        "YouTube": 'hsl(348, 97%, 39%)',
+        "Toggle": "hsl(230, 22%, 74%)",
+        // ToggleDarkFrom: 'hsl(210, 78%, 56%)',
+        // ToggleDarkTo: 'hsl(146, 68%, 55%)',
+        // ToggleLight: 'hsl(230, 22%, 74%)',
+        "Very-Dark-Blue": 'hsl(230, 17%, 14%)',
+        "Very-Dark-Blue-Top": 'hsl(232, 19%, 15%)',
+        "Dark-Desaturated-Blue": 'hsl(228, 28%, 20%)',
+        "Desaturated-Blue": 'hsl(228, 34%, 66%)',
+        "White": 'hsl(0, 0%, 100%)',
+        "Very-Pale-Blue": 'hsl(225, 100%, 98%)',
+        "Light-Grayish-Blue": 'hsl(227, 47%,  96%)',
+        "Dark-Grayish-Blue": 'hsl(228, 12%, 44%)'
+      },
+      fontFamily: {
+        'Inter': ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
